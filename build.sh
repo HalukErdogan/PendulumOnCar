18dec471601fffbde5110db101a01410436a7f22
Haluk Erdogan <haluk_erdogan@outlook.com>
2023-01-28 13:54:59 +0300

    improved the build.sh file, so it ask to clean ./build

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-mkdir build
+#!/bin/bash
+if [ -d "build" ]; then
+    read -p "Build directory already exists, would you like to delete it? (y/n): " choice
+    case "$choice" in 
+      y|Y ) rm -rf build; mkdir build;;
+      n|N ) echo "Continuing with existing build directory...";;
+      * ) echo "Invalid input, exiting..."; exit;;
+    esac
+else
+    mkdir build
+fi
 cd build
 cmake ..
-cmake --build .
+make
